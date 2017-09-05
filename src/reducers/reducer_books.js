082dc4d5f0307b0d905ea34b2a3d5bc5fcba4efe
Chris Andrews <chris.chase.andrews@gmail.com>
2017-09-04 22:00:34 -0400

    finished up book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: "JavaScript: The Good Parts", pages: 100},
+        {title: "Harry Potter", pages: 600},
+        {title: "C# 6.0 in a Nutshell", pages: 1000},
+        {title: "Kittens and Bowls: A Story of Sits", pages: 900},
+        {title: "Fucking T-Mobile; How I Grew To Hate Samsung", pages: 1},
+    ]
+}
